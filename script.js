587e3178e7f0adbce9ba1e5e8b3347d48d72926f
Faiz Ur Rehman <faizurrehman1415@gmail.com>
2025-05-01 23:03:22 +0530

    "about us ready"

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const links = document.querySelectorAll("li a");
 const currentPage = location.pathname.split("/").pop();
 
+
+
 links.forEach(link => {
   if (link.getAttribute("href") === currentPage) {
     link.classList.add("active");
@@ -8,10 +10,23 @@ links.forEach(link => {
 });
 
 const Btn = document.querySelector(".btn");
-    Btn.addEventListener("click", () => {
+
+Btn.addEventListener("click", () => {
     Btn.style.backgroundColor = "var(--darkblue)";
 });
-const Btn1 = document.querySelector(".btn-1");
-    Btn1.addEventListener("click", () => {
-    Btn1.style.backgroundColor = "var(--darkblue)";
+
+const btn1 = document.querySelector(".btn-1");
+btn1.addEventListener("click", () => {
+    btn1.style.backgroundColor = "var(--darkblue)";
 });
+
+const btn2 = document.getElementsByClassName("btn3");
+btn2.addEventListener("click", () => {
+    btn2.style.backgroundColor = "var(--darkblue)";
+    const about = document.querySelector(".aboutus-content");
+    console.log(about);
+    about.classList.add("animation"); 
+});
+
+
+
